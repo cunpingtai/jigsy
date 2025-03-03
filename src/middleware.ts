7c26eb39582a8341953bf6a4840a72599e1f615f
xianyuye237 <linweixian@cunpingtai.com>
2025-03-04 07:58:27 +0800

    feat: 添加功能

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,39 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+import { NextResponse } from "next/server";
+import acceptLanguage from "accept-language";
+import language from "../languages.json";
+
 const isPrivateRoute = createRouteMatcher(["/demo(.*)"]);
+
+const languages = language.languages;
+acceptLanguage.languages(languages);
 
 export default clerkMiddleware(async (auth, request) => {
   if (isPrivateRoute(request)) {
     await auth.protect();
   }
+
+  let lng;
+  if (!lng) lng = acceptLanguage.get(request.headers.get("Accept-Language"));
+  if (!lng) lng = language.fallbackLng;
+
+  if (
+    !languages.some((loc) => request.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !request.nextUrl.pathname.startsWith("/_next") &&
+    !request.nextUrl.pathname.startsWith("/api")
+  ) {
+    const locale = lng;
+
+    return NextResponse.redirect(
+      new URL(
+        `/${locale}${request.nextUrl.pathname}${request.nextUrl.search}`,
+        request.url
+      )
+    );
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
